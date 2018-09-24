9529555d9068283cd7d94180853015a4d189a7cf
apsteinmetz <apsteinmetz@yahoo.com>
2018-09-24 17:58:38 -0400

    save as notebook. loop through possible last digits

--- mathfun.r
+++ mathfun.r
@@ -6,12 +6,8 @@ output: html_notebook
 #Distribution of primes
 ```{r}
 library(RcppAlgos)
-primes<-RcppAlgos::primeSieve(10^10)
-last_digit<-primes %% 10
-follows_1<-last_digit[which(last_digit[1:1000000]==9)+1]
-hist(follows_1,breaks = c(1:10),freq=FALSE)
+system.time(primes<-RcppAlgos::primeSieve(10^10))
+system.time(last_digit<-primes %% 10)
+follows_1<-last_digit[which(last_digit[1:1000000]==1)+1]
+tmp<-hist(follows_1,breaks = c(0,2,4,6,8,10))
 ```
-
-Add a new chunk by clicking the *Insert Chunk* button on the toolbar or by pressing *Ctrl+Alt+I*.
-
-When you save the notebook, an HTML file containing the code and output will be saved alongside it (click the *Preview* button or press *Ctrl+Shift+K* to preview the HTML file).
